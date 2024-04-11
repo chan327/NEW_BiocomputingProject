4458c66d6b45dd65d05f5a672944e65c1f40d1d5
Kaitlyn Ying <156353832+kaitlynrying@users.noreply.github.com>
2024-04-11 19:08:02 -0400

    everything up to date, no full drugbank

--- Website/app.py
+++ Website/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='PyCharm')
 
+print('Testingggggg')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
