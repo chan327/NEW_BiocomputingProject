2cf2fcea66c553183aca328fcd60ab8660f7674d
Nikhil Sadavarte <sadavartenikhil@gmail.com>
2024-04-12 00:00:29 -0400

    everything up to date, no full drugbank

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='PyCharm')
 
-
+@app.route('/results')
+def results():
+    return render_template('results.html', name='PyCharm')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
