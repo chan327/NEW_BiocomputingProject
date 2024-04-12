cd974017c1b21910f6d675ee88438f602525d5ea
Adam Chan <chan327@purdue.edu>
2024-04-11 22:35:10 -0400

    chatgpt is my daddy

--- Storing information.py
+++ Storing information.py
@@ -12,7 +12,6 @@ def extract_data_from_line(line, tag):
 names = []
 cas_numbers = []
 descriptions = []
-smiles = []
 
 with open('full database.xml') as file:
     is_within_drug = False
@@ -20,14 +19,13 @@ with open('full database.xml') as file:
         # Check if we're entering a new drug entry
         if '<drug ' in line:
             is_within_drug = True
-            current_drug = {'name': None, 'cas_number': None, 'description': None, 'smiles': None}
+            current_drug = {'name': None, 'cas_number': None, 'description': None}
         elif '</drug>' in line and is_within_drug:
             # End of the current drug entry
             if all(value is not None for value in current_drug.values()):  # Ensure all fields are populated
                 names.append(current_drug['name'])
                 cas_numbers.append(current_drug['cas_number'])
                 descriptions.append(current_drug['description'])
-                smiles.append(current_drug['smiles'])
             is_within_drug = False
         elif is_within_drug:
             # Extract data if we're within a drug entry
@@ -37,9 +35,14 @@ with open('full database.xml') as file:
                 current_drug['cas_number'] = extract_data_from_line(line, 'cas-number')
             elif '<description>' in line and not current_drug['description']:
                 current_drug['description'] = extract_data_from_line(line, 'description')
-            elif '<kind>SMILES</kind>' in line and not current_drug['smiles']:
-                value = next(file)
-                current_drug['smiles'] = extract_data_from_line(value, 'value')
 
-print(smiles)
-#print(names, cas_numbers, descriptions)
+output_file_path = 'drugs_data.txt'
+
+# Write the data to the file
+with open(output_file_path, 'w') as file:
+    for name, cas_number, description in zip(names, cas_numbers, descriptions):
+        # Writing each drug's data followed by a delimiter line
+        file.write(f"Name: {name}\n")
+        file.write(f"CAS Number: {cas_number}\n")
+        file.write(f"Description: {description}\n")
+        file.write('-' * 50 + '\n')  # Delimiter line for readability
